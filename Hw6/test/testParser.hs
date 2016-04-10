bd7c8efbd320c9c5b4383055983a56e8dff1f8ea
Edgar Zhavoronkov <edzhavoronkov@gmail.com>
2016-04-10 15:44:35 +0300

    small tests

--- Hw6/test/testParser.hs
+++ Hw6/test/testParser.hs
@@ -4,20 +4,18 @@ import Parser
 import Expr
 
 testSimpleAssignmemnt :: Bool
-testSimpleAssignmemnt = True
--- testSimpleAssignmemnt = parseExpr "x := 1" == Assign (Var "x") (Num 1)
+testSimpleAssignmemnt = parseString "x := 1" == Assign (Var "x") (Num 1)
 
 testSmallProgram :: Bool
-testSmallProgram = True
--- testSmallProgram = parseExpr expr == ast
---     where
---         expr = "read x; if y + 1 == x then write y else skip"
---         ast = Colon
---                 (Read (Var "x"))
---                 (IfCond
---                     (BinOp Eq (BinOp Add (Var "y") (Num 1)) (Var "x"))
---                     (Write (Var "y"))
---                     Skip)
+testSmallProgram = parseString expr == ast
+    where
+        expr = "read x; if y + 1 == x then write y else skip"
+        ast = Colon
+                (Read (Var "x"))
+                (IfCond
+                    (BinOp Eq (BinOp Add (Var "y") (Num 1)) (Var "x"))
+                    (Write (Var "y"))
+                    Skip)
 
 main :: IO ()
 main = do
