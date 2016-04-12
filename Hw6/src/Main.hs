b293526c503e96ecc1fadf30dd0f52e65c8f8c09
edgarzhavoronkov <edzhavoronkov@gmail.com>
2016-04-12 18:51:40 +0300

    Pretty printer and tiny optimizer

--- Hw6/src/Main.hs
+++ Hw6/src/Main.hs
@@ -1,6 +1,8 @@
 import Parser
 import Expr
 
+import System.Environment
+
 showAST :: (String -> Stmt) -> String -> IO ()
 showAST parser input = do
     let ast = parser input
@@ -8,5 +10,13 @@ showAST parser input = do
 
 main :: IO ()
 main = do
-    input <- getContents
-    showAST parseString input
+    args <- getArgs
+    case args of
+        [] -> do
+            input <- getContents
+            showAST parseString input
+        ["-o"] -> do
+            input <- getContents
+            showAST (optimize . parseString) input
+            pprint $ optimize $ parseString input
+        _ -> putStrLn "Wrong key! Use -o for optimized mode"
--- Hw6/src/Parser.hs
+++ Hw6/src/Parser.hs
@@ -153,3 +153,78 @@ parseString str =
     case parse parser "" str of
         Left e -> error $ show e
         Right r -> r
+
+--Pretty printing
+pprint' :: Expr -> String
+pprint' (Var i) = i
+pprint' (Num n) = show n
+pprint' (BinOp Add e1 e2) = pprint' e1 ++ " + " ++ pprint' e2
+pprint' (BinOp Sub e1 e2) = pprint' e1 ++ " - " ++ pprint' e2
+pprint' (BinOp Mul e1 e2) = pprint' e1 ++ " * " ++ pprint' e2
+pprint' (BinOp Div e1 e2) = pprint' e1 ++ " / " ++ pprint' e2
+pprint' (BinOp Mod e1 e2) = pprint' e1 ++ " % " ++ pprint' e2
+pprint' (BinOp Eq e1 e2) = pprint' e1 ++ " == " ++ pprint' e2
+pprint' (BinOp Neq e1 e2) = pprint' e1 ++ " != " ++ pprint' e2
+pprint' (BinOp Gt e1 e2) = pprint' e1 ++ " > " ++ pprint' e2
+pprint' (BinOp Geq e1 e2) = pprint' e1 ++ " >= " ++ pprint' e2
+pprint' (BinOp Lt e1 e2) = pprint' e1 ++ " < " ++ pprint' e2
+pprint' (BinOp Leq e1 e2) = pprint' e1 ++ " <= " ++ pprint' e2
+pprint' (BinOp And e1 e2) = pprint' e1 ++ " && " ++ pprint' e2
+pprint' (BinOp Or e1 e2) = pprint' e1 ++ " || " ++ pprint' e2
+
+
+pprint :: Stmt -> IO ()
+pprint = helper ""
+    where
+        helper :: String -> Stmt -> IO ()
+        helper indent Skip =
+            putStr $ indent ++ "skip"
+        helper indent (Assign (Var v) e) =
+            putStr $ indent ++ v ++ " := " ++ pprint' e
+        helper indent (Colon s1 s2) = do
+            helper indent s1
+            putStrLn ";"
+            helper indent s2
+        helper indent (Write e) =
+            putStr $ indent ++ "write " ++ pprint' e
+        helper indent (Read e) =
+            putStr $ indent ++ "read " ++ pprint' e
+        helper indent (WhileLoop e s) = do
+            putStrLn $ indent ++ "while " ++ pprint' e ++ "do"
+            helper ('\t':indent) s
+        helper indent (IfCond e s1 s2) = do
+            putStrLn $ indent ++ "if " ++ pprint' e
+            putStrLn $ indent ++ "then"
+            helper ('\t' : indent) s1
+            putStrLn $ ('\n':indent) ++ "else"
+            helper ('\t' : indent) s2
+--TODO - bugfix
+simplify :: Expr -> Expr
+simplify (Var x) = Var x
+simplify (Num i) = Num i
+simplify (BinOp Add e (Num 0)) = e
+simplify (BinOp Add (Num 0) e) = e
+simplify (BinOp Sub e (Num 0)) = e
+simplify (BinOp Mul e (Num 1)) = e
+simplify (BinOp Mul (Num 1) e) = e
+simplify (BinOp Mul (Num 0) e) = Num 0
+simplify (BinOp Mul e (Num 0)) = Num 0
+simplify (BinOp Div (Num 0) e) = Num 0
+simplify (BinOp Div e (Num 1)) = e
+simplify (BinOp Mod (Num 0) e) = Num 0
+simplify (BinOp Mod e (Num 1)) = Num 0
+simplify (BinOp Add (Num x) (Num y)) = Num (x + y)
+simplify (BinOp Sub (Num x) (Num y)) = Num (x - y)
+simplify (BinOp Mul (Num x) (Num y)) = Num (x * y)
+simplify (BinOp Div (Num x) (Num y)) = Num (x `div` y)
+simplify (BinOp Mod (Num x) (Num y)) = Num (x `mod` y)
+simplify (BinOp op e1 e2) = BinOp op (simplify e1) (simplify e2)
+
+optimize :: Stmt -> Stmt
+optimize Skip = Skip
+optimize (Colon s1 s2) = Colon (optimize s1) (optimize s2)
+optimize (Assign e1 e2) = Assign e1 (simplify e2)
+optimize (Read e) = Read e
+optimize (Write e) = Write (simplify e)
+optimize (WhileLoop e s) = WhileLoop (simplify e) (optimize s)
+optimize (IfCond e s1 s2) = IfCond ((simplify . simplify) e) (optimize s1) (optimize s2)
